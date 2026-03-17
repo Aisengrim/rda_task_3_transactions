fb577856ddb44fe98326a6e370b099dd51c72ecc
aisen <djetsubo1337@gmail.com>
2026-03-17 18:12:42 +0200

    Solution 2.1.0

--- task.sql
+++ task.sql
@@ -1,9 +1,9 @@
 USE ShopDB;
 
-START TRANSACTION;
-
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
+
+START TRANSACTION;
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
 VALUES (1, 1, 1);
